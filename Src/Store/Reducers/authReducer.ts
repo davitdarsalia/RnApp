08b9dd29ef472d923bccad1ce88007a33ab35928
Davit Darsalia <darsalia.david1998@gmail.com>
2022-01-22 22:11:56 +0400

    feat: AuthReducer Added

--- Src/Store/Reducers/authReducer.ts
+++ Src/Store/Reducers/authReducer.ts
@@ -1,18 +1,41 @@
+import {UserAction} from './../Types/ActionTypes';
 import {AuthActions} from '../Types/AuthTypes';
 
 const initialState = {
     loginData: null,
     registerData: null,
+
+    loading: false,
+
+    errors: null,
 };
 
-export const userReducer = (state = initialState, action) => {
+export const userReducer = <K>(
+    state = initialState,
+    action: UserAction<any>,
+) => {
     switch (action.type) {
         case AuthActions.LOGIN_REQUEST:
-            return state;
+            return {
+                ...state,
+                loading: true,
+                errors: null,
+            };
         case AuthActions.LOGIN_SUCCESS:
-            return state;
+            return {
+                ...state,
+                loading: false,
+                errors: null,
+                registerData: {
+                    action,
+                },
+            };
         case AuthActions.LOGIN_FAILURE:
-            return state;
+            return {
+                ...state,
+                loading: false,
+                errors: action.payload,
+            };
         default:
             return state;
     }
