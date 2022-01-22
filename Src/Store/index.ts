fe22887b72298ef2312bf0c233e91888845f1fbb
Davit Darsalia <darsalia.david1998@gmail.com>
2022-01-22 22:17:53 +0400

    feat: Temporary Root Middleware Added

--- Src/Store/index.ts
+++ Src/Store/index.ts
@@ -1,4 +1,5 @@
 import {applyMiddleware, createStore} from 'redux';
 import thunk from 'redux-thunk';
+import {authReducer} from './Reducers/authReducer';
 
-export const store = createStore({}, applyMiddleware(thunk));
+export const store = createStore(authReducer, applyMiddleware(thunk));
