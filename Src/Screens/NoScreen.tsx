0db1bac594927784db97e6e3de3bb0d7592986aa
Davit Darsalia <darsalia.david1998@gmail.com>
2022-01-22 21:01:02 +0400

    feat: Initials

---
+++ Src/Screens/NoScreen.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+  View,
+  Button,
+  Text,
+  TextInput,
+  FlatList,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+
+interface Props {}
+
+export const NoScreen: React.FC<Props> = ({}) => {
+  return (
+    <View>
+      <Text>David</Text>
+    </View>
+  );
+};
