375bf75c3d15d43b29a1fda22f325313cf074289
Davit Darsalia <darsalia.david1998@gmail.com>
2022-01-22 23:40:15 +0400

    feat: Stacks Update + FeedStack <New>

---
+++ Src/Navigation/Stacks/FeedStack.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {RootStackScreenList} from '../Types';
+
+import MainScreen from '../../Screens/MainScreen';
+import AllHotelsScreen from '../../Screens/AllHotelsScreen';
+import HotelDetailScreen from '../../Screens/HotelDetailScreen';
+
+import {colors} from '../../consts/colors';
+
+const Stack = createStackNavigator<RootStackScreenList>();
+
+interface Props {}
+
+const FeedStack: React.FC<Props> = () => {
+    const {Navigator, Screen} = Stack;
+
+    return (
+        <Navigator initialRouteName="mainScreen">
+            {/* <Screen name="authLoading" component={} /> */}
+            <Screen
+                name="mainScreen"
+                component={MainScreen}
+                options={{
+                    title: 'Hotel Midnight',
+                    headerTintColor: colors.main,
+                    headerStyle: {
+                        backgroundColor: colors.background,
+                    },
+                }}
+            />
+            <Screen
+                name="allHotelsScreen"
+                component={AllHotelsScreen}
+                options={{
+                    title: 'All Hotels',
+                    headerTintColor: colors.main,
+                    headerBackTitle: ' ',
+                    headerStyle: {
+                        backgroundColor: colors.background,
+                    },
+                }}
+            />
+            <Screen
+                name="hotelDetailScreen"
+                component={HotelDetailScreen}
+                options={{
+                    title: ' ',
+                    headerTransparent: true,
+                    headerTintColor: colors.main,
+                    headerBackTitle: ' ',
+                }}
+            />
+        </Navigator>
+    );
+};
+
+export default FeedStack;
