5c2c12966ca9f7150a04712fcea4e0eceb4a14d2
Davit Darsalia <darsalia.david1998@gmail.com>
2022-01-22 22:46:31 +0400

    feat: RootReducer Moved To Store

--- Src/Store/index.ts
+++ Src/Store/index.ts
@@ -1,5 +1,6 @@
 import {applyMiddleware, createStore} from 'redux';
 import thunk from 'redux-thunk';
-import {authReducer} from './Reducers/authReducer';
 
-export const store = createStore(authReducer, applyMiddleware(thunk));
+import {RootReducer} from './Reducers/RootReducer';
+
+export const store = createStore(RootReducer, applyMiddleware(thunk));
