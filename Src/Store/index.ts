41c282038f01f31e5a4f87d7a072427179fa87b2
Davit Darsalia <darsalia.david1998@gmail.com>
2022-01-22 23:20:36 +0400

    feat: Store Config Modified + Persistency

--- Src/Store/index.ts
+++ Src/Store/index.ts
@@ -1,6 +1,37 @@
 import {applyMiddleware, createStore} from 'redux';
-import thunk from 'redux-thunk';
+import ReduxThunk from 'redux-thunk';
+
+import {persistStore, persistReducer} from 'redux-persist';
+import createFilter from 'redux-persist-transform-filter';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import {RootReducer} from './Reducers/RootReducer';
 
-export const store = createStore(RootReducer, applyMiddleware(thunk));
+const slice = createFilter('root', [
+    /*
+        Include Slices Here, F. E
+
+        'auth.permissions'
+    */
+]);
+
+const PersistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+    // Persist Reducers Goes Here
+    whiteList: [],
+    // Slices Of Persist Reducer Goes Here
+    transfroms: [],
+};
+
+const persistorReducer = persistReducer(PersistConfig, RootReducer);
+
+const store = createStore(persistorReducer, applyMiddleware(ReduxThunk));
+const persistor = persistStore;
+
+// Type For Custon App Dispatch
+export type RootStateType = ReturnType<typeof store.getState>;
+export type AppDispatchType = typeof store.dispatch;
+
+export {store, persistor};
