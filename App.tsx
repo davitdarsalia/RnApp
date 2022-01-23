1ad1aeecf413fbe90a6b2e3fad17e412e9da116d
Davit Darsalia <darsalia.david1998@gmail.com>
2022-01-23 15:34:54 +0400

    feat: Modified App.Ts <ToolKit>

--- App.tsx
+++ App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {View} from 'react-native';
 
 import {Provider} from 'react-redux';
-import {store} from './Src/Store';
+import {setupStore} from './Src/Store';
 
 import RootStack from './Src/Navigation/Stacks/RootStack';
 import {NavigationContainer} from '@react-navigation/native';
 
 import globalStyles from './styles';
+
+const store = setupStore();
 
 const App = () => {
     return (
