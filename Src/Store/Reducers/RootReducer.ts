afad936940c9d4e0d1d099bc79ea5ea34e5498c4
Davit Darsalia <darsalia.david1998@gmail.com>
2022-01-23 15:35:26 +0400

    style: Fix

--- Src/Store/Reducers/RootReducer.ts
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import {authReducer} from './AuthReducer';
-
-export const RootReducer = combineReducers({
-    auth: authReducer,
-});
-
-export type RootState = ReturnType<typeof RootReducer>;
