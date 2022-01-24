8c7794a3367c465826882057c80917bd4a096ca6
Davit Darsalia <darsalia.david1998@gmail.com>
2022-01-24 18:14:15 +0400

    feat: RootStack Modified

--- Src/Navigation/Stacks/RootStack.tsx
+++ Src/Navigation/Stacks/RootStack.tsx
@@ -2,12 +2,7 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {RootStackScreenList} from '../Types';
 
-import LoginScreen from '../../Screens/LoginScreen';
-import RegisterScreen from '../../Screens/RegisterScreen';
-import RegistrationScreen from '../../Screens/RegistrationScreen';
-
-import {colors} from '../../consts/colors';
-import FeedStack from './FeedStack';
+import TabStack from './TabStack';
 
 const Stack = createStackNavigator<RootStackScreenList>();
 
@@ -17,21 +12,12 @@ const RootStack: React.FC<Props> = () => {
     const {Navigator, Screen} = Stack;
 
     return (
-        <Navigator initialRouteName="feedStack">
-            {/* <Screen name="authLoading" component={} /> */}
-            <Screen name="feedStack" component={FeedStack} />
-            <Screen
-                name="registrationScreen"
-                component={RegistrationScreen}
-                options={{
-                    title: 'Registration / Login ',
-                    headerTransparent: true,
-                    headerTintColor: colors.main,
-                    headerBackTitle: ' ',
-                }}
-            />
-            <Screen name="loginScreen" component={LoginScreen} />
-            <Screen name="registerScreen" component={RegisterScreen} />
+        <Navigator
+            initialRouteName="tabStack"
+            screenOptions={{
+                headerShown: false,
+            }}>
+            <Screen name="tabStack" component={TabStack} />
         </Navigator>
     );
 };
