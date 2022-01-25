f0e49ee1a5fddb26c6811a0ecfdd747a852a5817
Davit Darsalia <darsalia.david1998@gmail.com>
2022-01-25 16:14:11 +0400

    feat: RegStyle

---
+++ Src/containers/registrationContainer/style.tsx
@@ -0,0 +1,11 @@
+import {StyleSheet, Dimensions} from 'react-native';
+
+const {width, height} = Dimensions.get('window');
+
+const st = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
+
+export default st;
