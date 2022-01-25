c40ef8bfeda5dda82cb3ae62ed939f4056067b43
Davit Darsalia <darsalia.david1998@gmail.com>
2022-01-25 15:04:19 +0400

    feat: TabBar Changes

--- Src/Navigation/Stacks/TabStack.tsx
+++ Src/Navigation/Stacks/TabStack.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {TabStackScreenList} from '../Types';
+
 import FeedStack from './FeedStack';
 import MapStack from './MapStack';
 import AuthScreen from '../../Screens/Auth';
+
+import {TouchableOpacity} from 'react-native-gesture-handler';
+
+import {MaterialIcons} from '@expo/vector-icons';
+import {FontAwesome} from '@expo/vector-icons';
+import {Entypo} from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator<TabStackScreenList>();
 
@@ -11,21 +18,62 @@ const TabStack = () => {
     const {Navigator, Screen} = Tab;
     return (
         <Navigator
-            screenOptions={{
+            screenOptions={({route}) => ({
                 headerShown: false,
-            }}>
+                tabBarIcon: ({focused, color, size}) => {
+                    let icon;
+
+                    if (route.name === 'Feed') {
+                        focused
+                            ? (icon = (
+                                  <MaterialIcons
+                                      name="dynamic-feed"
+                                      size={36}
+                                      color="teal"
+                                  />
+                              ))
+                            : (icon = (
+                                  <MaterialIcons
+                                      name="dynamic-feed"
+                                      size={25}
+                                      color="black"
+                                  />
+                              ));
+                    } else if (route.name === 'Map') {
+                        focused
+                            ? (icon = (
+                                  <FontAwesome
+                                      name="map"
+                                      size={36}
+                                      color="teal"
+                                  />
+                              ))
+                            : (icon = (
+                                  <FontAwesome
+                                      name="map"
+                                      size={25}
+                                      color="black"
+                                  />
+                              ));
+                    } else if (route.name === 'Auth') {
+                        focused
+                            ? (icon = (
+                                  <Entypo name="users" size={36} color="teal" />
+                              ))
+                            : (icon = (
+                                  <Entypo
+                                      name="users"
+                                      size={25}
+                                      color="black"
+                                  />
+                              ));
+                    }
+
+                    return <TouchableOpacity>{icon}</TouchableOpacity>;
+                },
+            })}>
             <Screen name="Feed" component={FeedStack} />
 
-            {/* <Screen
-                name="registrationScreen"
-                component={RegistrationScreen}
-                options={{
-                    title: 'Registration / Login ',
-                    headerTransparent: true,
-                    headerTintColor: colors.main,
-                    headerBackTitle: ' ',
-                }}
-            /> */}
             <Screen name="Map" component={MapStack} />
             <Screen name="Auth" component={AuthScreen} />
         </Navigator>
