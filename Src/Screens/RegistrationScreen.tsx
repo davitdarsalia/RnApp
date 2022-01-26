42ba8843770d46493c80c6570855a42da87274c0
Davit Darsalia <darsalia.david1998@gmail.com>
2022-01-26 14:10:31 +0400

    feat: Action Created

--- Src/Screens/RegistrationScreen.tsx
+++ Src/Screens/RegistrationScreen.tsx
@@ -4,12 +4,33 @@ import {View} from 'react-native';
 import globalStyles from '../../styles';
 import RegistrationContainer from '../containers/registrationContainer/RegistrationContainer';
 
+import {UseCustomDispatch} from '../Store/Hooks/CustomDispatch';
+import {registrationAction} from '../Store/Reducers/authReducer';
+
 interface Props {}
 
 const RegistrationScreen: React.FC<Props> = ({}) => {
+    const dispatch = UseCustomDispatch();
+
+    const onRegistration = (
+        fullName: string,
+        password: string,
+        email: string,
+        checked: string,
+    ) => {
+        dispatch(
+            registrationAction({
+                fullName,
+                password,
+                email,
+                checked,
+            }),
+        );
+    };
+
     return (
         <View style={globalStyles.mainScreen}>
-            <RegistrationContainer />
+            <RegistrationContainer onReg={onRegistration} />
         </View>
     );
 };
