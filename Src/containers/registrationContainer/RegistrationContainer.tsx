ae2f252354f746a4f645b1f077fec6a740c9b3b8
Davit Darsalia <darsalia.david1998@gmail.com>
2022-01-26 11:02:30 +0400

    feat: Form

--- Src/containers/registrationContainer/RegistrationContainer.tsx
+++ Src/containers/registrationContainer/RegistrationContainer.tsx
@@ -2,18 +2,51 @@ import React, {useState} from 'react';
 import {Button, SafeAreaView} from 'react-native';
 
 import {CustomInput} from '../../components/customInput/CustomInput';
-import {registration} from '../../Types/GlobalInterfaces';
 import st from './style';
+
+import {Formik} from 'formik';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
 interface Props {}
 
 const RegistrationContainer: React.FC<Props> = ({}) => {
     return (
         <SafeAreaView style={st.container}>
-            <CustomInput />
-            <CustomInput />
+            <Formik
+                initialValues={{
+                    email: '',
+                    password: '',
+                }}
+                onSubmit={(values) => {
+                    console.log(values.email);
+                    console.log(values.password);
+                }}
+                // validationSchema={NewPasswordSchema}
+            >
+                {({
+                    handleSubmit,
+                    values,
+                    setFieldTouched,
+                    setFieldValue,
+                    errors,
+                }) => {
+                    return (
+                        <KeyboardAwareScrollView
+                            keyboardShouldPersistTaps="always"
+                            showsVerticalScrollIndicator={false}
+                            scrollEnabled
+                            enableOnAndroid>
+                            <CustomInput />
+                            <CustomInput />
 
-            <Button title="Register" onPress={() => {}} />
+                            <Button
+                                onPress={() => handleSubmit}
+                                title="Register"
+                            />
+                        </KeyboardAwareScrollView>
+                    );
+                }}
+            </Formik>
         </SafeAreaView>
     );
 };
