ccc11ab409adf63f673afffa3e5c5a667dabe244
Davit Darsalia <darsalia.david1998@gmail.com>
2022-01-26 22:34:40 +0400

    feat: Styles Modified

--- Src/components/customInput/style.tsx
+++ Src/components/customInput/style.tsx
@@ -9,13 +9,19 @@ const st = StyleSheet.create({
     inputStyle: {
         width: width * 0.9,
         height: 50,
-        borderColor: '#0096FF',
+        borderColor: '#8E2DE2',
         backgroundColor: 'rgba(0,0,0,.1)',
         alignSelf: 'center',
         paddingHorizontal: 30,
-        borderWidth: 1,
+        borderWidth: 2.5,
         borderRadius: 5,
         marginVertical: 10,
+        position: 'relative',
+    },
+    passwordIcon: {
+        position: 'absolute',
+        top: -50,
+        right: 20,
     },
 });
 
