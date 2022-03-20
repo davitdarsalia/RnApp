04b0ffdcd4dd0a38cda63d410e7611f02f95fbac
davitdarsalia <darsalia.david1998@gmail.com>
2022-03-20 22:04:51 +0400

    feat: Middleware Demo <Interrupted>

--- Src/Middleware/AppMiddleware.ts
+++ Src/Middleware/AppMiddleware.ts
@@ -1,58 +1,58 @@
 import {normalize} from 'normalizr';
-import axios, {AxiosRequestHeaders, AxiosResponse} from 'axios';
-import qs from 'qs';
-import Config from 'react-native-config';
-import {middlewareReturnProps} from './Types';
+// import axios, {AxiosRequestHeaders, AxiosResponse} from 'axios';
+// import qs from 'qs';
+// import Config from 'react-native-config';
+// import {middlewareReturnProps} from './Types';
 
-const defaultHeaders: AxiosRequestHeaders = {
-   'Content-Type': 'application/json',
-};
+// const defaultHeaders: AxiosRequestHeaders = {
+//    'Content-Type': 'application/json',
+// };
 
-// Base Urls Fetched From .Env
-const BASE_URL = Config.BASE_URL;
+// // Base Urls Fetched From .Env
+// const BASE_URL = Config.BASE_URL;
 
-export const callApi = async ({
-   endpoint,
-   schema,
-   method,
-   headers = {},
-   body,
-   params = {},
-   config = {},
-   path,
-   userToken,
-   externalCall,
-}: Props): Promise<middlewareReturnProps> => {
-   const options = {
-      baseURL: externalCall ? '' : BASE_URL,
-      headers: {
-         ...defaultHeaders,
-         ...headers,
-         ...headers(userToken ? {Authorization: `Bearer ${userToken}`} : {}),
-      },
-   };
+// export const callApi = async ({
+//    endpoint,
+//    schema,
+//    method,
+//    headers = {},
+//    body,
+//    params = {},
+//    config = {},
+//    path,
+//    userToken,
+//    externalCall,
+// }: Props): Promise<middlewareReturnProps> => {
+//    const options = {
+//       baseURL: externalCall ? '' : BASE_URL,
+//       headers: {
+//          ...defaultHeaders,
+//          ...headers,
+//          ...headers(userToken ? {Authorization: `Bearer ${userToken}`} : {}),
+//       },
+//    };
 
-   const axiosInstance: Record<string, any> = axios.create(options);
+//    const axiosInstance: Record<string, any> = axios.create(options);
 
-   let query = '';
+//    let query = '';
 
-   if (body) {
-      Object.keys(body).forEach((key) => {
-         if (body[key] === undefined) delete body[key];
-      });
-   }
+//    if (body) {
+//       Object.keys(body).forEach((key) => {
+//          if (body[key] === undefined) delete body[key];
+//       });
+//    }
 
-   let configParams = [{...config}];
+//    let configParams = [{...config}];
 
-   if (path) {
-      query += '/' + path.join('/');
-   }
+//    if (path) {
+//       query += '/' + path.join('/');
+//    }
 
-   if (params) {
-      query += qs.stringify(params, {addQueryPrefix: true});
-   }
+//    if (params) {
+//       query += qs.stringify(params, {addQueryPrefix: true});
+//    }
 
-   if (['post', 'patch', 'put', 'delete'].includes(method)) {
-      configParams = [body, {...params, ...config}];
-   }
-};
+//    if (['post', 'patch', 'put', 'delete'].includes(method)) {
+//       configParams = [body, {...params, ...config}];
+//    }
+// };
