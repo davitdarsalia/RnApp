2fa0ea124a693a87e58093daa5ae78f49e552495
davitdarsalia <darsalia.david1998@gmail.com>
2022-03-27 02:37:32 +0400

    feat: Navigation Types

--- Src/Navigation/types.ts
+++ Src/Navigation/types.ts
@@ -1,6 +1,8 @@
+import {StackNavigationProp} from '@react-navigation/stack';
+
 export type RouteList = {
-   AuthStack: AuthScreenList;
-   MainStack: BottomTabsNavigator;
+   AuthStack: undefined;
+   MainStack: undefined;
 };
 
 export type AuthScreenList = {
@@ -13,3 +15,13 @@ export type BottomTabsNavigator = {
    Loans: undefined;
    Statistics: undefined;
 };
+
+export type AuthStackGenericProp<T extends keyof AuthScreenList & string> =
+   StackNavigationProp<AuthScreenList, T>;
+
+export type BottomTabsGenericProp<
+   T extends keyof BottomTabsNavigator & string,
+> = StackNavigationProp<BottomTabsNavigator, T>;
+
+export type RootStackGenericProp<T extends keyof RouteList> =
+   StackNavigationProp<RouteList, T>;
