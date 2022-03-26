969c1a74b0f5e2fe63d3ea98e8a14dc6629a7970
davitdarsalia <darsalia.david1998@gmail.com>
2022-03-27 02:35:24 +0400

    style: Styles for Modal

---
+++ Src/Components/BlurGestureModal/style.tsx
@@ -0,0 +1,21 @@
+import {StyleSheet, Dimensions} from 'react-native';
+
+const {width, height} = Dimensions.get('screen');
+
+export const styles = StyleSheet.create({
+   outerWrapper: {
+      flex: 1,
+      alignItems: 'center',
+   },
+
+   innerWrapper: {
+      paddingVertical: width * 0.1,
+      paddingHorizontal: width * 0.1,
+      width: width,
+      height: height,
+      backgroundColor: 'rgba(0,0,0,.3)',
+      position: 'absolute',
+      left: -(width * 0.05),
+      zIndex: 100,
+   },
+});
