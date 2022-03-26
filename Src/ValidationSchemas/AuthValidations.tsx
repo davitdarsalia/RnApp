19a6937fd281422bcb464f70a0817d986d65dae1
davitdarsalia <darsalia.david1998@gmail.com>
2022-03-27 02:38:24 +0400

    feat: New Validation Schema

--- Src/ValidationSchemas/AuthValidations.tsx
+++ Src/ValidationSchemas/AuthValidations.tsx
@@ -4,3 +4,7 @@ export const registrationSchema = Yup.object({
    firstName: Yup.string().required().min(1).max(35),
    lastName: Yup.string().required().min(1).max(100),
 });
+
+export const searchValue = Yup.object({
+   searchValue: Yup.string().required().min(1).max(50),
+});
