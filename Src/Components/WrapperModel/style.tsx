b673246eb22e5dd6ee3e87df95a6e9536f46c602
davitdarsalia <darsalia.david1998@gmail.com>
2022-05-28 00:46:57 +0400

    Reset

---
+++ Src/Components/WrapperModel/style.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+import { RHeight, RWidth } from "../../Generics/ResponsiveUnits";
+
+export const LabelStyles = StyleSheet.create({
+  modalWrapper: {
+    width: RWidth(100),
+    height: RHeight(60),
+    backgroundColor: "red",
+  },
+});
